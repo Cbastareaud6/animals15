25804066da8e0d79d2a17d0bae89c0366c6faa54
Cathy Bastareaud <catybastareaud@gmail.com>
2022-01-27 17:01:07 -0700

    Initial commit; fix dependencies in progress

---
+++ app/src/main/java/edu/cnm/deepdive/animals15/model/Animal.java
@@ -0,0 +1,65 @@
+package edu.cnm.deepdive.animals15.model;
+
+import com.google.gson.annotations.Expose;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.UUID;
+
+public class Animal {
+
+  @Expose
+  private UUID uuid;
+
+  @Expose
+  private String created;
+
+  @Expose
+  private String title;
+
+  @Expose
+  @SerializedName("href")
+  private String imageUrl;
+
+  @Expose
+  private String description;
+
+  public UUID getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(UUID uuid) {
+    this.uuid = uuid;
+  }
+
+  public String getCreated() {
+    return created;
+  }
+
+  public void setCreated(String created) {
+    this.created = created;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
