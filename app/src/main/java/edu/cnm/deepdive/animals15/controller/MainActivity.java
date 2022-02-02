cf31d511fc41f54fb860659c6c79e119b18c4bf3
Cathy Bastareaud <catybastareaud@gmail.com>
2022-02-02 11:58:23 -0700

    Render image in progress

--- app/src/main/java/edu/cnm/deepdive/animals15/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals15/controller/MainActivity.java
@@ -1,10 +1,11 @@
 package edu.cnm.deepdive.animals15.controller;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import edu.cnm.deepdive.animals15.R;
 import edu.cnm.deepdive.animals15.model.Animal;
 import edu.cnm.deepdive.animals15.service.WebServiceProxy;
@@ -22,31 +23,21 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     animalSelector = findViewById(R.id.animal_selector);
-    new Retriever().start();
+
 
   }
-
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
 
     @Override
-    public void run() {
+    protected List<Animal> doInBackground(Void... voids) {
       try {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals()
             .execute();
         if (response.isSuccessful()) {
           Log.d(getClass().getName(), response.body().toString());
-          List<Animal> animals = response.body();
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              adapter = new ArrayAdapter<>(
-                  MainActivity.this, R.layout.item_animal_spinner, animals);
-              adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-              animalSelector.setAdapter(adapter);
 
-            }
-          });
+          return response.body();
         } else {
           Log.e(getClass().getName(), response.message());
         }
@@ -55,7 +46,22 @@ public class MainActivity extends AppCompatActivity {
         Log.e(getClass().getName(), e.getMessage(), e);
 
       }
+      return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(List<Animal> animals) {
+      super.onPostExecute(animals);
+
+          adapter = new ArrayAdapter<>(
+              MainActivity.this, R.layout.item_animal_spinner, animals);
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+          animalSelector.setAdapter(adapter);
+    }
+
 
     }
   }
-}
+
+
